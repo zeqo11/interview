bb34fd818197d8c643d16f130930b5c09a4e06ca
Taylor Fuqua <>
2019-02-07 14:52:33 -0500

    remove unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+ 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to the interview app!</h2>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+   
         </header>
       </div>
     );
